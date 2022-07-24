eab6c98f6e49c2cca48e676fe9dd7f3890c1e663
aa-buraev@bk.ru <aa-buraev@bk.ru>
2022-07-24 14:56:28 +0300

    Добавили решение задачи №10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите три числа от 0 до 9: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int[] array = { a, b, c };
+Console.WriteLine($"{array[0]}{array[1]}{array[2]}");
+Console.WriteLine($"{array[1]}");
+
