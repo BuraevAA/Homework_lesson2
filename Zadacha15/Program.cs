59decaa4ebc1a90fd284378cfe2e3d7bbca6e4ff
aa-buraev@bk.ru <aa-buraev@bk.ru>
2022-07-25 21:52:39 +0300

    Добавили решение задачи №15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число, соответствующе дню недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string a = "Да, это выходной день!";
+string b = "Нет, увы это будний день!";
+if(num >= 1 && num <= 5)
+{
+    Console.Write($"{num} {b}");
+}
+else if(num >= 6 && num <= 7)
+{
+    Console.Write($"{num} {a}");
+}
+
+
